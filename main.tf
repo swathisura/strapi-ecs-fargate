279c4cfec42b10dea07f6f49ff156d574905c8b1
swathisura <swathisura65@gmail.com>
2026-02-19 18:46:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,10 +53,26 @@ resource "aws_ecs_cluster" "strapi_cluster" {
 }
 
 # -------------------------------
-# IAM ROLE (assumes existing ECS execution role)
+# ECS TASK EXECUTION ROLE
 # -------------------------------
-data "aws_iam_role" "ecs_task_execution_role" {
+resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # -------------------------------
@@ -88,7 +104,7 @@ resource "aws_ecs_task_definition" "strapi_task" {
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
