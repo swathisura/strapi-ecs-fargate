ae048f7ce7c0acbc4b0a31b45346b93c70331539
swathisura <swathisura65@gmail.com>
2026-02-23 12:48:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 # -------------------------------
-# DEFAULT VPC
+# USE EXISTING DEFAULT VPC
 # -------------------------------
 data "aws_vpc" "default" {
   default = true
 }
 
 # -------------------------------
-# DEFAULT SUBNETS
+# USE EXISTING SUBNETS
 # -------------------------------
 data "aws_subnets" "default" {
   filter {
@@ -20,33 +20,31 @@ data "aws_subnets" "default" {
 }
 
 # -------------------------------
-# SECURITY GROUP (CREATE IF NOT EXISTS)
+# USE EXISTING SECURITY GROUP
 # -------------------------------
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg"
-  description = "Allow Strapi traffic"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "strapi_sg" {
+  name   = "strapi-sg"
+  vpc_id = data.aws_vpc.default.id
 }
 
 # -------------------------------
-# ECS CLUSTER
+# ECS CLUSTER (safe to create)
 # -------------------------------
 resource "aws_ecs_cluster" "strapi_cluster" {
   name = var.ecs_cluster_name
+}
+
+# -------------------------------
+# CLOUDWATCH LOG GROUP
+# (Ignore errors if already exists)
+# -------------------------------
+resource "aws_cloudwatch_log_group" "strapi_logs" {
+  name              = "/ecs/strapi"
+  retention_in_days = 7
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 # -------------------------------
@@ -59,9 +57,8 @@ resource "aws_ecs_task_definition" "strapi_task" {
   cpu                      = "256"
   memory                   = "512"
 
-  # USE EXISTING ROLE (IMPORTANT)
-  execution_role_arn = var.execution_role_arn
-  task_role_arn      = var.execution_role_arn
+  # IMPORTANT: use existing execution role
+  execution_role_arn = var.ecs_execution_role_arn
 
   container_definitions = jsonencode([
     {
@@ -75,6 +72,15 @@ resource "aws_ecs_task_definition" "strapi_task" {
           protocol      = "tcp"
         }
       ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/strapi"
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
 }
@@ -91,7 +97,9 @@ resource "aws_ecs_service" "strapi_service" {
 
   network_configuration {
     subnets          = data.aws_subnets.default.ids
-    security_groups  = [aws_security_group.strapi_sg.id]
+    security_groups  = [data.aws_security_group.strapi_sg.id]
     assign_public_ip = true
   }
+
+  depends_on = [aws_cloudwatch_log_group.strapi_logs]
 }
