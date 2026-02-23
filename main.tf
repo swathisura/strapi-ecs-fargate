b11b3ac2755912c786d21153ed9d529365de75be
swathisura <swathisura65@gmail.com>
2026-02-23 13:16:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,22 +20,36 @@ data "aws_subnets" "default" {
 }
 
 # -------------------------------
-# EXISTING SECURITY GROUP (shared account)
+# SECURITY GROUP (CREATE UNIQUE)
 # -------------------------------
-data "aws_security_group" "strapi_sg" {
-  name   = "strapi-sg"
+resource "aws_security_group" "strapi_sg" {
+  name   = "swathi-strapi-sg"
   vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # -------------------------------
 # ECS CLUSTER
 # -------------------------------
 resource "aws_ecs_cluster" "strapi_cluster" {
-  name = var.ecs_cluster_name
+  name = "swathi-strapi-cluster"
 }
 
 # -------------------------------
-# EXISTING CLOUDWATCH LOG GROUP
+# USE EXISTING LOG GROUP
 # -------------------------------
 locals {
   log_group_name = "/ecs/strapi"
@@ -45,13 +59,13 @@ locals {
 # ECS TASK DEFINITION
 # -------------------------------
 resource "aws_ecs_task_definition" "strapi_task" {
-  family                   = var.ecs_task_family
+  family                   = "swathi-strapi-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
 
-  # Use existing role (shared account)
+  # Existing role from shared account
   execution_role_arn = var.ecs_execution_role_arn
 
   container_definitions = jsonencode([
@@ -80,10 +94,10 @@ resource "aws_ecs_task_definition" "strapi_task" {
 }
 
 # -------------------------------
-# ECS SERVICE
+# ECS SERVICE (UNIQUE NAME)
 # -------------------------------
 resource "aws_ecs_service" "strapi_service" {
-  name            = "strapi-service"
+  name            = "swathi-strapi-service-v3"
   cluster         = aws_ecs_cluster.strapi_cluster.id
   task_definition = aws_ecs_task_definition.strapi_task.arn
   desired_count   = 1
@@ -91,7 +105,7 @@ resource "aws_ecs_service" "strapi_service" {
 
   network_configuration {
     subnets         = data.aws_subnets.default.ids
-    security_groups = [data.aws_security_group.strapi_sg.id]
+    security_groups = [aws_security_group.strapi_sg.id]
     assign_public_ip = true
   }
 }
